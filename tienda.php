1eda3a488d3f9ad505f92e686b0a5f1e33ff88a5
jessicalucio64 <93625032+jessicalucio64@users.noreply.github.com>
2022-05-03 10:35:13 -0500

    Add files via upload
    nuevo

--- tienda.php
+++ tienda.php
@@ -1,11 +1,11 @@
 <?php
 
 class persona{
-    public $nombre;
-    public $apellido;
-    public $edad;
-    public $direccion;
-    public $rfc;
+    private $nombre;
+    private $apellido;
+    private $edad;
+    private $direccion;
+    private $rfc;
 
     function constructorPersona($nombrec, $apellidoc, $edadc, $direccionc, $rfcc){
         $this->nombre=$nombrec;
@@ -14,6 +14,36 @@ class persona{
         $this->direccion=$direccionc;
         $this->rfc=$rfcc;
 
+    }
+    function setnombre($nombrec){
+        $this->nombre = $nombrec;
+    }
+    function getnombre(){
+        return $this->nombre;
+    }
+    function setapellido($apellidoc){
+        $this->apellido = $apellidoc;
+    }
+    function getapellido(){
+        return $this->apellido;
+    }
+    function setedad($edadc){
+        $this->edad = $edadc;
+    }
+    function getedad(){
+        return $this->edad;
+    }
+    function setdireccion($direccionc){
+        $this->direccion = $direccionc;
+    }
+    function getdireccion(){
+        return $this->direccion;
+    }
+    function setrfc($rfcc){
+        $this->rfc = $rfcc;
+    }
+    function getrfc(){
+        return $this->rfc;
     }
 }
 class empleado extends persona{
@@ -30,6 +60,33 @@ class empleado extends persona{
         $this->turno=$turnoc;
 
     }
+    function setno_Empleado($no_Empleadoc){
+        $this->no_Empleado = $no_Empleadoc;
+    }
+    function getno_Empleado(){
+        return $this->no_Empleado;
+    }
+    function settiempo_trabajando($tiempo_trabajandoc){
+        $this->tiempo_trabajando = $tiempo_trabajandoc;
+    }
+    function gettiempo_trabajando(){
+        return $this->tiempo_trabajando;
+    }
+    function setfecha_contratación($fecha_contrataciónc){
+        $this->fecha_contratación = $fecha_contrataciónc;
+    }
+    function getfecha_contratación(){
+        return $this->fecha_contratación;
+    }
+    function setturno($turnoc){
+        $this->turno = $turnoc;
+    }
+    function getturno(){
+        return $this->turno;
+    }
+
+
+
 
 
 }
@@ -43,6 +100,7 @@ class tienda{
     private $turno;
     private $número_empleados;
     private $comisión;
+
     function constructortienda($rfcc,$nombrec,$turnoc,$número_empleadosc,$comisiónc){
 
         $this->rfc=$rfcc;
@@ -51,6 +109,40 @@ class tienda{
         $this->número_empleados=$número_empleadosc;
         $this->comisión=$comisiónc;
     }
+    function setrfc($rfcc){
+        $this->rfc = $rfcc;
+    }
+    function getrfc(){
+        return $this->rfc;
+    }
+    function setnombre($nombrec){
+        $this->nombre = $nombrec;
+    }
+    function getnombre(){
+        return $this->nombre;
+    }
+    function setturno($turnoc){
+        $this->turno = $turnoc;
+    }
+
+    function getturno(){
+        return $this->turno;
+    }
+    function setnúmero_empleados($número_empleadosc){
+        $this->número_empleados = $número_empleadosc;
+    }
+
+    function getnúmero_empleados(){
+        return $this->número_empleados;
+    }
+    function setcomisión($comisiónc){
+        $this->comisión = $comisiónc;
+    }
+
+    function getcomisión(){
+        return $this->comisión;
+    }
+
 }
 
 class mascota{
@@ -72,4 +164,56 @@ class mascota{
         $this->precioVenta=$precioVentac;
 
     }
+    function setespecie($especiec){
+        $this->especie = $especiec;
+    }
+
+    function getespecie(){
+        return $this->especie;
+    }
+    function setcolor($colorc){
+        $this->color = $colorc;
+    }
+
+    function getcolor(){
+        return $this->color;
+    }
+    function setedad($edadc){
+        $this->edad = $edadc;
+    }
+
+    function getedad(){
+        return $this->edad;
+    }
+    function setfecha_llegada($fecha_llegadac){
+        $this->fecha_llegada = $fecha_llegadac;
+    }
+
+    function getfecha_llegada(){
+        return $this->fecha_llegada;
+    }
+    function setcantidad($cantidadc){
+        $this->cantidad = $cantidadc;
+    }
+
+    function getcantidad(){
+        return $this->cantidad;
+    }
+    function setprecio_Compra($precio_Comprac){
+        $this->precio_Compra = $precio_Comprac;
+    }
+
+    function getprecio_Compra(){
+        return $this->precio_Compra;
+    }
+    function setprecioVenta($precioVentac){
+        $this->precioVenta = $precioVentac;
+    }
+
+    function getprecioVenta(){
+        return $this->precioVenta;
+    }
+
+
+
 }
